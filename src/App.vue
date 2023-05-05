3cb1835fc60568e63de1fc7c2ff45d71ffd6e841
Tania Vásquez <103429138+taniasadith@users.noreply.github.com>
2023-05-04 20:01:18 -0500

    main structure

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     
     <AppHeader></AppHeader>
+    <AppMain></AppMain>
     <AppFooter></AppFooter>
   </div>
 </template>
@@ -10,12 +11,11 @@
 
 import AppHeader from "@/components/layout/AppHeader.component.vue"
 import AppFooter from "@/components/layout/AppFooter.component.vue"
-
-
+import AppMain from './components/layout/AppMain.component.vue'
 
 export default {
   name: 'App',
-  components: {AppFooter,AppHeader},
+  components: {AppFooter,AppHeader,AppMain},
 
 }
 </script>
---
+++ src/components/layout/AppMain.component.vue
@@ -0,0 +1,24 @@
+<template>   
+    <div class="card">
+        <div class="flex card-container blue-container overflow-hidden">
+            <div class="flex-none flex align-items-center justify-content-center bg-blue-500 font-bold text-white m-2 px-5 py-3 border-round">
+                PrimeFlex
+            </div>
+            <div class="flex-grow-1 flex align-items-center justify-content-center bg-blue-500 font-bold text-white m-2 px-5 py-3 border-round">
+                PrimeFlex
+            </div>
+            <div class="flex-none flex align-items-center justify-content-center bg-blue-500 font-bold text-white m-2 px-5 py-3 border-round">
+                PrimeFlex
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+
+<style scoped>
+
+</style>

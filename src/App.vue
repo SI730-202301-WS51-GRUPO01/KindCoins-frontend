2e2028b18762271b6c04766213ec2aea4146066c
DiegoMerinoMas <u202111473@upc.edu.pe>
2023-05-05 22:54:30 -0500

    Router view prot

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@ import AppFooter from "@/components/layout/AppFooter.component.vue"
 import CampaignCreated from "@/components/campaign-section/launch-campaign/CampaignCreated.component.vue";
 import SelectMethod from "@/components/payment-methods/SelectMethod.component.vue";
 import PaymentMethod from "@/components/payment-methods/PaymentMethod.component.vue";
+import { RouterView } from "vue-router";
 export default {
   name: 'App',
   components: { AppFooter, AppHeader, CampaignCreated, SelectMethod, PaymentMethod },

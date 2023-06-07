1bab2c76fb9a55ba01d6e5c8d2eb2598a8cbdfa6
EnriqueLLatasFlores <102637887+EnriqueLLatasFlores@users.noreply.github.com>
2023-06-06 23:08:09 -0500

    Update CampaignData.component.vue

--- src/components/campaign-section/launch-campaign/CampaignData.component.vue
+++ src/components/campaign-section/launch-campaign/CampaignData.component.vue
@@ -1,219 +1,142 @@
-<template>
-
     <template>
-    </template>
-        <div id="div-camps"> 
-            <span id="first-sect" style=" font-family: 'Roboto';color: #909399;">Campañas /</span>
-            <span style="font-family: 'Roboto';color: #131414;">Crear campaña </span>
-        </div>
-    
-        <div id="box-content">
-            <div id="img">
-                <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSCmB96dGtBtE_H4kXPD5PgRrFUuyNI7w_edfXYV73VFJp86UPy" id="flecha">
-            </div>
-    
-            <div id="pasos">
-                <p id="paso">Paso 1 de 5</p>
-            </div>
-            <div id="text-descrip">
-                <p id="descrip">Nombre de la campaña</p>
-            </div>
-    
-            <div id="text-area">
-                <Textarea id="text-box" v-model="value" rows="5" cols="75"></Textarea>
-            </div>
-            <div id="text-selec">
-                <p id="selec">Seleccione los tipos de donaciones que desea recibir</p>
-            </div>
-            <div id="text-meta">
-                <p id="meta">Meta</p>
-            </div>
-            <div id="area-username">
-                <span class="p-float-label">
-                <Textarea v-model="value" :textarea="{style: {marginTop: '1.5em'}}" rows="3" cols="75"></Textarea>
-                <label>Ingrese cantidad de coins</label>
-                </span>
-            </div>
-            <div>
-                <router-link to="/campaign-details">
-                    <button id="boton-siguiente">SIGUIENTE</button>
+        <nav class="navdonaciones" >
+          <pv-breadcrumb  :model="breadcrumbItems" ></pv-breadcrumb>
+        </nav>
+         
+        <div class="caja">    
+                <div class="content" style="margin:25px"> 
+                    <div class="flex flex-column gap-2" style="margin: 10px; width: 80%;">
+                    <p style="text-align: center;  color: #687781"> PASO 1 DE 5 </p>
+                    <label for="username" class="txtbutton">Nombre de la campaña</label>
+                    
+                <pv-input-text id="username" v-model="value" aria-describedby="username-help" />
+                    
+                    
+                </div>
+
+                <div class="flex flex-column gap-2" style="margin: 10px; width: 80%;">
+                    <label for="username" class="txtbutton">Slogan</label>
+                    <pv-input-text id="username" v-model="value" aria-describedby="username-help" />
+                    
+                </div>
+                <div style="width: 130px;">
+                    <p class="txtbutton"> Foto de campaña </p>
+                </div>
+                <div class="img-box" style="display: grid;grid-template-columns: 25% 75%; width: 70%;">
+                    <div>
+                
+                    <img src="../../../../public/triangulo.jpg" style="width: 100%;height: 100%;">
+                
+                        
+                        </div>
+                    <div  style="margin-left:30px;">
+                        <div >
+                            <pv-button label="Editar" style="background-color:#409EFF;"/>
+                        </div>
+                        <div style="display: flex;">
+                            <img src="public\doc.jpg" style="width: 35px;height: 35px;">
+                            <p  >foto001.jpg</p>
+                        </div>
+                    </div> 
+
+                </div>
+                <small >Formato jpg/png,maximo 500kb</small>
+                
+
+                <div class="hola" >
+                <router-link to="/donation-details"> 
+                    <pv-button  label="siguiente" style="background: #FFDE59;" />
                 </router-link>
             </div>
-        </div>
-        <!-- <div id="card-img">
-            <p id="text-campañas">
-                Ultimas campañas
-            </p>
-            <img id="lupita" src="../../../../public/lupa.png" alt="lupa">
-            <pv-card id="card-palestin" style="width: 20em">
-            <template #header>
-                <img src="../../../../public/niñacomiendo.jpg" alt="sequia" width="320" height="258"/>
-            </template>
-            <template #title> Apoya a las familias afectadas por la sequía en el Cuerno de África </template>
-            <template #content>
-                <p>
-                    Las comidas compartidas proporcionarán apoyo en efectivo de emergencia a las familias afectadas por la
-                    sequía en Etiopía, Kenia y Somalia.
-                </p>
-            </template>
-            <template #footer>
-                <button id="donar-palestin">DONAR AHORA</button>
-            </template>
-        </pv-card>
-        </div>
-     -->
-    </template>
-    
-    <script></script>
-    
-    <style>
-    #div-camps{
-            box-sizing: border-box;
-            display: inline-block   ;
-            flex-direction: row;
-            align-items: flex-start;
-            padding: 10px 40px;
-            gap: 40px;
-            position: relative;
-            width: 80%;
-            height: 44px;
-            left: 15%;
-            top: 25px;
-            /* Border/base */
-            border: 1px solid #DCDFE6;
-            border-radius: 8px;
+        
+
+            </div>
+
+            <div class="dere" > 
+            <p style="margin-left:20%;margin-bottom:20px ;" class="txtbutton"> Ultimas campañas</p>
+                <pv-card style="width: 100%;margin-left:16% ;">
+                <template #header>
+                    <img  alt="user header" src="public\card1.jpg" style="width: 100%;height: 200px;"/>
+                </template>
+                <template #title> Alimenta familias en Palestina </template>
+                <template #content>
+                    <p>
+                        Las Comidas compartidas proporcionarán a las familias de palestina
+                        un apoyo en efectivo para que puedan comprar alimentos en tiendas locales
+                    </p>
+                </template>
+                <template #footer>
+                    <pv-button label="Donar Ahora"  style="background: #0597A6;"/>
+                </template>
+            </pv-card>
+
+
+
+            </div>
+      </div>
+
+
+  </template>
+  
+  <script>
+
+
+  import Breadcrumb from 'primevue/breadcrumb';
+  
+  export default {
+    components: {
+      Breadcrumb,
+    },
+    data() {
+      return {
+        breadcrumbItems: [
+          { label: 'Campañas', url: '/',disabled:true }, //pasar los path
+          { label: 'Crear Campañas', url: '/campaign-details' },
+        //en caso este en costruccion lo desabilitamos jjiji
+        ],
+      };
+    },
+  };
+  </script>
+
+
+  <style>
+
+    .navdonaciones{
+        margin: 10px auto;
+        width: 80%;
     }
-    #box-content{
-        position: absolute;
-        width: 670px;
-        height: 531px;
-        left: 353px;
-        top: 220px;
-        background: #FFFFFF;
-        border-radius: 5px;
-    }
-    #flecha{
-        width: 20px;
-        margin-left: 30px;
-        margin-top: 35px;
-    }
-    #paso{
-        position: absolute;
-        top: -25px;
-        left: 290px;
-        width: 74px;
-        height: 24px;
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 14px;
-        line-height: 24px;
-        display: flex;
-        align-items: center;
-        color: #687781;
-    }
-    #text-box{
-        left: 38px;
-        top: 25px;
-    }
-    #descrip{
-        left:40px;
-        top:20px;
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 14px;
-        line-height: 24px;
+    .txtbutton{
         color: #000000;
-    }
-    #selec{
-        top: 35px;
-        left: 40px;
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 14px;
-        line-height: 24px;
-        display: flex;
-        color: #000000;
-    }
-    #meta{
-        top: 100px;
-        left: 40px;
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 14px;
-        line-height: 24px;
-        display: flex;
-        color: #000000;
-    }
-    textarea {
-      line-height: 1.5;
-      padding-top: 1em;
-    }
-    #area-username{
-        left:37px;
-        top:110px;
-    }
-    #donar-palestin{
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        padding: 0px 16px;
-        gap: 16px;
         font-family: 'Roboto';
         font-style: normal;
         font-weight: 500;
-        font-size: 14px;
-        color: #FFFFFF;
-        line-height: 36px;
-        background: #0597A6;
-        border: none;
-        border-radius: 4px;
-        flex: none;
-        order: 0;
-        flex-grow: 0;
-        cursor: pointer;
+        font-size: 15px;
+        line-height: 18px;
     }
-    #card-palestin{
-        position: absolute;
-        left: 70%;
-        top: 200px;
+
+    .caja{
+        background-color: white;
+        width: 90%;
+        height: calc(78vh + 40px);
+
+padding-left: calc(13vw + 40px);
+        display: grid;
+        grid-template-columns:  60% 30%;
     }
-    #card-img{
-        left: -200px;
-        top:-20px;
+        .hola{
+           margin-left: 65%;
+        }
+    
+    @media (max-width: 768px) {
+        /* Cambiar a una columna cuando la pantalla sea más pequeña que 768px */
+        .caja {
+        grid-template-columns: auto;
+        }
+        .hola{
+            margin-left: 56%;
     }
-    #boton-siguiente{
-        left:480px;
-        top: 120px;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        padding: 0px 16px;
-        gap: 16px;
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 500;
-        font-size: 14px;
-        color: #FFFFFF;
-        line-height: 36px;
-        background: #FFDE59;
-        border: none;
-        border-radius: 4px;
-        flex: none;
-        order: 0;
-        flex-grow: 0;
-        cursor: pointer;
+
     }
-    #text-campañas{
-        left:1333px;
-        top: 150px;
-    }
-    #lupita{
-        left: 1623px;
-        width: 25px;
-        top:125px;
-    }
-    </style>
+
+</style>

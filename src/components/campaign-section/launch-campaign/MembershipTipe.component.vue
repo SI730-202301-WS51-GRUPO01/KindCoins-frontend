a568449cc79c5b4ac9b368ac266fb8d021a4572e
Tania Vásquez <103429138+taniasadith@users.noreply.github.com>
2023-06-10 11:32:35 -0500

    fix

--- src/components/campaign-section/launch-campaign/MembershipTipe.component.vue
+++ src/components/campaign-section/launch-campaign/MembershipTipe.component.vue
@@ -1,39 +1,12 @@
 <template>
-    <div class="card">
-        <div class="flex flex-column justify-content-center flex-wrap p-5 " id="Planes">
-              
-            <!-- Plan gratuito -->
-                      
-              <div class="flex flex-row card-container justify-content-between  flex-wrap border-round-2xl p-5">              
-                <div>
-                  <p class="tittle-plan">Mensual</p>
-                  <ul class="text-start">
-                    <li>Crea tu campaña y recibe donanciones</li>
-                    <li>Duración ilimitada</li>
-                    <li>Mayor visibilidad en nuestra sección de campañas</li>
-                    <li>Crea tu campaña y recibe donanciones</li>
-                    <li>Proridad en soporte</li>
-                  </ul>
-                </div>
-                
-                  <pv-button text @click="visible = true">
-                  <p class="price align-self-center font-bold text-xl">S/ 19.99</p>
-                  </pv-button>
-                  
-                  <pv-dialog v-model:visible="visible" modal header="Header" >
-                    <SelectMethod/>
-                    <template #footer>
-                      <pv-button text @click="visible = false">Cerrar</pv-button>
-                    </template>
-                  </pv-dialog>
-                
-              </div>
-
+  <div class="card">
+      <div class="flex flex-column justify-content-center flex-wrap p-5 " id="Planes">
             
-            <br>
-            <div class="flex flex-row card-container justify-content-between  flex-wrap border-round-2xl p-5 ">
+          <!-- Plan gratuito -->
+                    
+            <div class="flex flex-row card-container justify-content-between  flex-wrap border-round-2xl p-5">              
               <div>
-                <p class="tittle-plan">Anual</p>
+                <p class="tittle-plan">Mensual</p>
                 <ul class="text-start">
                   <li>Crea tu campaña y recibe donanciones</li>
                   <li>Duración ilimitada</li>
@@ -42,29 +15,56 @@
                   <li>Proridad en soporte</li>
                 </ul>
               </div>
-              <pv-button text>
-                <div class="align-self-center">
-                  <p class="price font-bold text-xl text-right">S/ 215.99</p>
-                  <p class="color-descuento"><u>10% de descuento</u></p>
-                </div>
-              </pv-button>
-            </div>
-            <br>
-            <div class="flex flex-row card-container justify-content-between  flex-wrap border-round-2xl p-5 ">
-              <div>
-                <p class="tittle-plan">Free</p>
-                <ul class="text-start">
-                  <li>Crea tu campaña y recibe donanciones</li>
-                  <li>Duración máxima de campaña de 1 mes</li>
-                </ul>
-              </div>
-              <pv-button text>
-                <p class="price align-self-center font-bold text-xl">Gratis</p>
-              </pv-button>
+              
+                <pv-button text @click="visible = true">
+                <p class="price align-self-center font-bold text-xl">S/ 19.99</p>
+                </pv-button>
+                
+                <pv-dialog v-model:visible="visible" modal header="Header" >
+                  <SelectMethod/>
+                  <template #footer>
+                    <pv-button text @click="visible = false">Cerrar</pv-button>
+                  </template>
+                </pv-dialog>
+              
             </div>
 
-        </div>
-    </div>
+          
+          <br>
+          <div class="flex flex-row card-container justify-content-between  flex-wrap border-round-2xl p-5 ">
+            <div>
+              <p class="tittle-plan">Anual</p>
+              <ul class="text-start">
+                <li>Crea tu campaña y recibe donanciones</li>
+                <li>Duración ilimitada</li>
+                <li>Mayor visibilidad en nuestra sección de campañas</li>
+                <li>Crea tu campaña y recibe donanciones</li>
+                <li>Proridad en soporte</li>
+              </ul>
+            </div>
+            <pv-button text>
+              <div class="align-self-center">
+                <p class="price font-bold text-xl text-right">S/ 215.99</p>
+                <p class="color-descuento"><u>10% de descuento</u></p>
+              </div>
+            </pv-button>
+          </div>
+          <br>
+          <div class="flex flex-row card-container justify-content-between  flex-wrap border-round-2xl p-5 ">
+            <div>
+              <p class="tittle-plan">Free</p>
+              <ul class="text-start">
+                <li>Crea tu campaña y recibe donanciones</li>
+                <li>Duración máxima de campaña de 1 mes</li>
+              </ul>
+            </div>
+            <pv-button text>
+              <p class="price align-self-center font-bold text-xl">Gratis</p>
+            </pv-button>
+          </div>
+
+      </div>
+  </div>
 </template>
 
 <script>
@@ -73,13 +73,13 @@ import PaymentMethod from "../../payment-methods/PaymentMethod.component.vue";
 import SelectMethod from "../../payment-methods/SelectMethod.component.vue";
 
 export default {
-  name: 'MembershipType',
-  components: {PaymentMethod, SelectMethod},
-  
-  data() {
-  return {
-    visible: false,
-  };
+name: 'MembershipType',
+components: {PaymentMethod, SelectMethod},
+
+data() {
+return {
+  visible: false,
+};
 } ,
 
 }
@@ -88,21 +88,21 @@ export default {
 
 <style scoped>
 .card-container{
-  background-color: #EBFFFD;
+background-color: #EBFFFD;
 }
 .tittle-plan{
-    color: #0597A6;
-    font-weight: bold;
-    font-size: 1.25rem;
+  color: #0597A6;
+  font-weight: bold;
+  font-size: 1.25rem;
 }
 .text-start{
-  font-size: 0.875rem;
+font-size: 0.875rem;
 }
 .price{
-  color: #00DDE3;
+color: #00DDE3;
 }
 .color-descuento{
-  color: red;
-  font-size: 0.75rem;
+color: red;
+font-size: 0.75rem;
 }
 </style>

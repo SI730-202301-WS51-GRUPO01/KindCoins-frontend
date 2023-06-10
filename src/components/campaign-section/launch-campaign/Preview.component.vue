9b5ff3ea81fcb2c232dcc4e8666797f3ff8cb55d
Tania Vásquez <103429138+taniasadith@users.noreply.github.com>
2023-06-10 16:53:37 -0500

    fix: preview

--- src/components/campaign-section/launch-campaign/Preview.component.vue
+++ src/components/campaign-section/launch-campaign/Preview.component.vue
@@ -1,109 +1,114 @@
 <template>
-  <div>
+  <div class="m-5">
+    <div class="pt-5 pb-3 flex justify-content-center">
+        <h3 class="font-bold">Paso 5 de 5</h3>
+    </div>
     <!-- Wrapper Card -->
-    <Card class="wrapper-card">
-      <template #title>
-        <div class="pv-text-center">
-          <h4 style="font-weight: bold">Vista previa</h4>
-        </div>
-      </template>
+    <div class="flex">
+        <Card class="wrapper-card">
+        <template #title>
+            <div class="pv-text-center">
+            <h4 style="font-weight: bold">Vista previa</h4>
+            </div>
+        </template>
 
-      <template #content>
-        <!-- First Part -->
-        <transition name="slide-fade">
-          <Card class="campaign-card">
-            <template #title>
-              <div>
-                <img
-                  class="campaign-image"
-                  src="public\niñacomiendo.jpg"
-                  alt="Campaign Image"
-                />
-                <span class="text-center" style="font-weight: bold"
-                  >Alimenta familias en Palestina</span
-                >
-              </div>
-            </template>
-
-            <template #content>
-              <div class="line">
-                <div class="left">
-                  <i class="pi pi-heart p-mr-1"></i>
-                  <span>Goal: $1000</span>
+        <template #content>
+            <!-- First Part -->
+            <transition name="slide-fade">
+            <Card class="campaign-card">
+                <template #title>
+                <div>
+                    <img
+                    class="campaign-image"
+                    src="https://i.postimg.cc/YS2zVqWF/v-img.png"
+                    alt="Campaign Image"
+                    />
+                    <span class="text-center" style="font-weight: bold"
+                    >Alimenta familias en Palestina</span
+                    >
                 </div>
-                <div class="right">
-                  <i class="pi pi-user p-ml-1"></i>
-                  <span>Donor: 100</span>
+                </template>
+
+                <template #content>
+                <div class="line">
+                    <div class="left">
+                    <i class="pi pi-heart p-mr-1"></i>
+                    <span>Goal: $1000</span>
+                    </div>
+                    <div class="right">
+                    <i class="pi pi-user p-ml-1"></i>
+                    <span>Donor: 100</span>
+                    </div>
                 </div>
-              </div>
 
-              <ProgressBar
-                :value="progressValue"
-                class="progress-bar"
-              ></ProgressBar>
-              <div class="line">
-                <div class="left">
-                  <span>$500 collected</span>
+                <ProgressBar
+                    :value="progressValue"
+                    class="progress-bar"
+                ></ProgressBar>
+                <div class="line">
+                    <div class="left">
+                    <span>$500 collected</span>
+                    </div>
+                    <div class="right">
+                    <span>Completion: {{ progressValue }}%</span>
+                    </div>
                 </div>
-                <div class="right">
-                  <span>Completion: {{ progressValue }}%</span>
+                </template>
+            </Card>
+            </transition>
+
+            <!-- Second Part -->
+            <transition name="slide-fade">
+            <Card class="summary-card">
+                <template #title>Resumen</template>
+
+                <template #content>
+                <div class="p-d-flex p-jc-between">
+                    <h3 class="text-center" style="font-weight: bold">
+                    Las comidas compartidas proporcionarán a las familias de
+                    Palestina un apoyo en efectivo para que puedan comprar
+                    alimentos en tiendas locales.
+                    </h3>
+                    <p class="text-center">
+                    Dos tercios de la población de Gaza —alrededor de 1.3 millones
+                    de personas— no pueden alimentar a sus familias debido a
+                    décadas de conflicto. Los disturbios políticos en la Franja de
+                    Gaza y los bloqueos continuos en el mar, la tierra y el aire a
+                    lo largo de Cisjordania dejan a la región dependiente de las
+                    importaciones. Esto significa que Palestina es muy vulnerable
+                    a las crisis globales y al aumento de los precios de los
+                    alimentos. Como uno de sus mayores importadores de harina de
+                    trigo y aceite, la guerra en Ucrania ha llevado al aumento de
+                    los precios de los alimentos en Palestina - un 3.7% de aumento
+                    en un año. El Programa Mundial de Alimentos está sobre el
+                    terreno proporcionando apoyo en efectivo a 300.000 palestinos
+                    vulnerables cada mes. La cuestión en Palestina no es la falta
+                    de alimentos, sino que la gente no puede permitirse comprarla.
+                    Es por eso que el WFP ofrece asistencia en efectivo para que
+                    las familias puedan obtener los alimentos que necesitan
+                    mientras impulsan la economía local. Pueden gastar su dinero
+                    en más de 300 tiendas designadas en Cisjordania y Gaza. El WFP
+                    prioriza a las familias encabezadas por mujeres, a las
+                    personas con discapacidad y a las personas mayores con apoyo.
+                    </p>
+                    <img
+                    class="summary-image"
+                    src="https://i.postimg.cc/T3db2N3M/Carousel.png"
+                    alt="Campaign Summary Image"
+                    />
                 </div>
-              </div>
-            </template>
-          </Card>
-        </transition>
+                </template>
+            </Card>
+            </transition>
 
-        <!-- Second Part -->
-        <transition name="slide-fade">
-          <Card class="summary-card">
-            <template #title>Resumen</template>
-
-            <template #content>
-              <div class="p-d-flex p-jc-between">
-                <h3 class="text-center" style="font-weight: bold">
-                  Las comidas compartidas proporcionarán a las familias de
-                  Palestina un apoyo en efectivo para que puedan comprar
-                  alimentos en tiendas locales.
-                </h3>
-                <p class="text-center">
-                  Dos tercios de la población de Gaza —alrededor de 1.3 millones
-                  de personas— no pueden alimentar a sus familias debido a
-                  décadas de conflicto. Los disturbios políticos en la Franja de
-                  Gaza y los bloqueos continuos en el mar, la tierra y el aire a
-                  lo largo de Cisjordania dejan a la región dependiente de las
-                  importaciones. Esto significa que Palestina es muy vulnerable
-                  a las crisis globales y al aumento de los precios de los
-                  alimentos. Como uno de sus mayores importadores de harina de
-                  trigo y aceite, la guerra en Ucrania ha llevado al aumento de
-                  los precios de los alimentos en Palestina - un 3.7% de aumento
-                  en un año. El Programa Mundial de Alimentos está sobre el
-                  terreno proporcionando apoyo en efectivo a 300.000 palestinos
-                  vulnerables cada mes. La cuestión en Palestina no es la falta
-                  de alimentos, sino que la gente no puede permitirse comprarla.
-                  Es por eso que el WFP ofrece asistencia en efectivo para que
-                  las familias puedan obtener los alimentos que necesitan
-                  mientras impulsan la economía local. Pueden gastar su dinero
-                  en más de 300 tiendas designadas en Cisjordania y Gaza. El WFP
-                  prioriza a las familias encabezadas por mujeres, a las
-                  personas con discapacidad y a las personas mayores con apoyo.
-                </p>
-                <img
-                  class="summary-image"
-                  src="public\niñacomiendo.jpg"
-                  alt="Campaign Summary Image"
-                />
-              </div>
-            </template>
-          </Card>
-        </transition>
-
-        <!-- Third Part -->
-        <Button label="Publish" class="publish-button" @click="visible = true" />
-        <pv-dialog v-model:visible="visible" modal >
-            <EndCampaignCreation/>
-        </pv-dialog>
-      </template>
-    </Card>
+            <!-- Third Part -->
+            <Button label="Publish" class="publish-button" @click="visible = true" />
+            <pv-dialog v-model:visible="visible" modal >
+                <EndCampaignCreation/>
+            </pv-dialog>
+        </template>
+        </Card>
+    </div>
   </div>
 </template>
 
@@ -133,7 +138,7 @@ export default {
 .wrapper-card {
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   border: 1px solid lightgray;
-  padding: 20px;
+  padding: 10px;
   width: 80%;
   /**center the */
   margin: auto;
